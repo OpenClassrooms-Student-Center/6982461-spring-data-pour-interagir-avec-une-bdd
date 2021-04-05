1edf79972e50e7415d5e32bb253d1720ed607289
romainsessa <romainsessa@gmail.com>
2021-04-05 11:10:04 +0200

    project structure generated with start.spring.io

---
+++ dataLayer/src/main/java/com/openclassrooms/datalayer/DataLayerApplication.java
@@ -0,0 +1,13 @@
+package com.openclassrooms.datalayer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataLayerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataLayerApplication.class, args);
+	}
+
+}
---
+++ dataLayer/src/test/java/com/openclassrooms/datalayer/DataLayerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.openclassrooms.datalayer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataLayerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
